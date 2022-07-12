36525ee337d4d4383d8b4437285fb192b3606e51
SVN <svn20070809@gmail.com>
2022-07-12 08:09:19 +0300

    НАстроил запуск - как не понял

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
+    /**
+     * @param args
+     */
     public static void main(String[]args) {
         // 12345
+        // 67890
         System.out.println("Hello, world");
     }
 }
